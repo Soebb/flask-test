ee65007a9959e17b7496827a403639b9326f7e3f
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-06 21:16:38 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
+from stream import create_stream_url
+from py_youtube import Data
+from flask import Flask, request, render_template
+
 app = Flask(__name__)
 
+default_thumb = "https://github.com/Soebb/own-utube-stream-link-gen/raw/main/default_thumbnail.jpg"
+
 @app.route('/')
-def hello_world():
-   return 'Hello, World!'
+def home():
+   return 'Hello, my function is creating YouTube stream url.'
