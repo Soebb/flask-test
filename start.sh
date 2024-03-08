a7930d98c8e178213c01117ccecb71cef8e804cc
Soebb <87156166+Soebb@users.noreply.github.com>
2024-03-09 01:23:20 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /app
 echo "----- Now trying to run gunicorn ------ " 
-gunicorn -b :5000 --reload --access-logfile - --error-logfile - app:app 
+gunicorn -b 0.0.0.0:5000 --reload --access-logfile - --error-logfile - app:app 
